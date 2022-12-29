06ddd965dee80ac40ba029effb1c3f645d23fec0
KUNDAN <KUNDAN@KUNDAN>
2022-12-29 17:22:48 +0530

    pushin the code

---
+++ src/test/java/scripts/Test1.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void Tc01() {
+		Reporter.log("hello jenkins",true);
+	}
+		
+	}
+
---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void Tc03() {
+		Reporter.log("hello java",true);
+	}
+		
+	}
+
+
