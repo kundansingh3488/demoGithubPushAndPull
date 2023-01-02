1d707bcd81ad56383fd19e474f1df6934072731d
KUNDAN <KUNDAN@KUNDAN>
2023-01-02 16:50:07 +0530

    new commit

---
+++ src/test/java/scripts/Test8.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test8 {
+	@Test
+	public void Tc08() {
+		Reporter.log("hello jenkins",true);
+	}
+		
+}
