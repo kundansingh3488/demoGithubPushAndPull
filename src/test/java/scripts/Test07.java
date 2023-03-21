ea27c731d9bcdd73286211dffdbfa2b856826de7
kundansingh3488 <ksingh3488@gmail.com>
2023-03-21 20:34:46 +0530

    hello

---
+++ src/test/java/scripts/Test07.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test07 {
+    @Test
+    public void Tc07() {
+        Reporter.log("hello dataBase01  testing",true);
+    }
+}
+
